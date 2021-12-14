a174c49afee9c5543c26901516f51f0b8e9be9a0
Tony <tonyliu05@hotmail.com>
2021-12-13 16:41:46 -0800

    print code

--- Final/q5.asm
+++ Final/q5.asm
@@ -26,5 +26,14 @@ waitloop:
     sub	$s0, $s0, 48		    # s0 = v0-48
     mul $s0, $s0, $t1           # s0 = s0 * v0
     div $t1, $t1, 10            # t1 = t1/10
-    add	$s1, $s1, $s0		    # s1= s1 + s0 
-    beq  $t1, $zero, done       #if is the zero go to print
+    add	$s1, $s1, $s0		    # s1 = s1 + s0 
+    beq $t1, $zero, done        #if is the zero go to print
+
+done:
+	
+    move	$a0, $s1	        #copy from register to register for print
+    li      $v0, 1              # system call code for print integers.
+    syscall 
+
+    li      $v0,10              # code 10 == exit
+    syscall                     # Return to OS.
