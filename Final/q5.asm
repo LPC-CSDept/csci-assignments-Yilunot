0948d93f2b79417116d34f2b2eb397e98eaf4cb4
Tony <tonyliu05@hotmail.com>
2021-12-13 16:13:39 -0800

    polling method

--- Final/q5.asm
+++ Final/q5.asm
@@ -16,3 +16,9 @@ digits:.word   100     # (one, ten, hundred) taking the 3 digits
 main:
     lw  $t1, digists    #load word to $t1, number of digist(3)
     lui $t0, 0ffff      #load upper immediate 
+
+waitloop:
+    lw   $t2, 0($t0)            #load word from the input control register
+    andi $t2, $t2, 0x0001       #reset or clear all the bits except LSb
+    beq  $t2, $zero, waitloop   #if the bit is not ready yet go back to the waitloop
+    lw   $v0, 4($t0)            #input device is ready so read a character from I/O
