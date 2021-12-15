02ef89fa6b16fba19def3cb57592529b30a96ee5
Tony <tonyliu05@hotmail.com>
2021-12-15 01:22:30 -0800

    enable all interrupts

--- Final/q6.asm
+++ Final/q6.asm
@@ -10,8 +10,23 @@
         .kdata
 
 #value holding
-s1:     .word 10              
-s2:     .word 11    
+s1:     .word 10         # storage for register $v0     
+s2:     .word 11         # storage for register $a0
+
 new:    .asciiz     "\n"
 
+#Section that contains instructions and program logic
+        .text     
+		.globl     main    
+
+main:   
+#Coprocessor 0 register $12; starus register
+
+        mfc0    $a0, $12     	    #read from the status register     
+		ori     $a0,   0xff11     	#enable all interrupts     
+		mtc0 	$a0, $12     	    #write back to the status register
+
+		lui     $t0, 0xFFFF     	#$t0 =   0xFFFF0000     
+		ori     $a0, $0, 2     	    #enable keyboard interrupt     
+		sw     	$a0, 0($t0)     	#write back to 0xFFFF0000;Receiver Control
 
